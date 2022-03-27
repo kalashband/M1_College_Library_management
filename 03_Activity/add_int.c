133fea6c76984573b689ddb68928802bda3da53e
kalashband <Kalashband90@gmail.com>
2022-03-28 00:08:56 +0530

    first commit

---
+++ 03_Activity/add_int.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int a=0;
+    int b=0;
+
+    printf("enter two number");
+    scanf("%d %d",&a,&b);
+
+    int Sum= a + b;
+    printf("sum = %d", Sum );
+    
+return 0;
+}
