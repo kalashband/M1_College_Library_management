26e8f554112394084c7ab6d16bf569a9d98cda69
kalashband <Kalashband90@gmail.com>
2022-03-28 00:11:00 +0530

    change  add number

--- 03_Activity/add_int.c
+++ 03_Activity/add_int.c
@@ -5,11 +5,11 @@ int main()
 {
     int a=0;
     int b=0;
-
+    int c=0;
     printf("enter two number");
-    scanf("%d %d",&a,&b);
+    scanf("%d %d %d",&a,&b,&c);
 
-    int Sum= a + b;
+    int Sum= a + b +c;
     printf("sum = %d", Sum );
     
 return 0;
